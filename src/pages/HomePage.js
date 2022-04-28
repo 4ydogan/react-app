f8563c2c50dcb19828f7f1939f9dc5ec92317623
4ydogan <mustafaaydogan12@outlook.com>
2022-04-29 01:46:33 +0300

    Some updates

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import { Navbar, NavbarBrand } from 'reactstrap'
+import UserListComponent from '../components/UserListComponent'
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div>HomePage</div>
+      <div>
+        <Navbar color='light' expand="md" light>
+          <div>
+            <NavbarBrand href='/'>React-Intro</NavbarBrand>
+          </div>
+         </Navbar>
+         <UserListComponent />
+      </div>
     )
   }
 }
