b3efc6871880c374737fb22f9380d3422f826452
4ydogan <mustafaaydogan12@outlook.com>
2022-04-29 03:04:18 +0300

    Some updates

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import { Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import { v4 as uuidv4 } from 'uuid';
 
 export default class FormComponent extends Component {
 
     constructor(props){
       super(props);
       this.state = {
-        id : uuid4(),
+        id : uuidv4(),
         name : "",
         surname : "",
         username : ""
@@ -16,17 +17,25 @@ export default class FormComponent extends Component {
   render() {
     return (
     <Modal fade={false} isOpen={this.props.visible}>
-      <ModalHeader>Do you want user?</ModalHeader>
+      <ModalHeader>User Informations</ModalHeader>
         <ModalBody>
           <Form>
             <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input id="exampleEmail" name="email" placeholder="with a placeholder" type="email"/>
+              <Label for="name">Name</Label>
+              <Input id="name" name="name" type="text" onChange={(e) => this.setState({name : e.target.value})}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="surname">Surname</Label>
+              <Input id="surname" name="surname" type="text"onChange={(e) => this.setState({surname : e.target.value})}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="username">Username</Label>
+              <Input id="username" name="username" type="text" onChange={(e) => this.setState({username : e.target.value})}/>
             </FormGroup>
           </Form>
         </ModalBody>
       <ModalFooter>
-        <button className='btn btn-success'>Add</button>
+        <button className='btn btn-success' }>Add</button>
         <button className='btn btn-danger' onClick={() => this.props.hide()}>Cancel</button>
       </ModalFooter>
     </Modal>
